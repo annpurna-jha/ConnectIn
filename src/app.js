fc2894904abd7db743e0dada98ba3232ac25fa7b
annpurna-jha <annpurnajha4@gmail.com>
2024-11-20 15:25:05 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ const express = require("express");
 const app = express(); //creating instance of express js application or new web server
 
 //handle incoming request
-app.use("/test",(req,res)=>{ // request handler
-    res.send("Hello from server")
+
+// this use() will get all HTTP methods 
+app.use("/",(req,res)=>{ // as this route handler start with / anything after this will get the same result . it will override all other route and give same result everytime.  for ex - /test
+    res.send("Hello annpurna")
 })
-app.use("/hello",(req,res)=>{ // request handler
-    res.send("Hello hello!")
+
+app.use("/test",(req,res)=>{ // as this route handler start with /test anything after this will get the same result . for ex - /test/xyz
+    res.send("Hello from server")
 })
 
 app.listen(7777,()=>{ // server listen on 7777 port no
