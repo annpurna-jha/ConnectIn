d0ff6880bf533702cb9e5931e72ef753748da8e5
annpurna-jha <annpurnajha4@gmail.com>
2024-12-02 09:09:28 +0530

    add-auth-midlleware in diff file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 
 const app = express();
-
+const {adminAuth, userAuth} = require('./middlewares/auth')
 // use of middleware - with help of this we don't have to write login of authentication again and again
-app.use("/admin",(req,res,next)=>{
-    const token ="xyz";
-    const isAdminAuthorized = token =="xyz";
-    if(!isAdminAuthorized) res.status(401).send("Unauthorized request");
-    else next();
-})
+app.use("/admin",adminAuth); // auth for all routes /admin
+
+app.get("/user/getAllData",userAuth,(req,res)=>{ //when we don't want auth in all /user route
+    res.send("All data sent");
+ })
+app.get("/user/signup",(req,res)=>{  // if we do auth check like /admin then auth check will happen in signup as well which is not needed
+    res.send("User signup successfully");
+ })
 
 app.get("/admin/getAllData",(req,res)=>{
    res.send("All data sent");
