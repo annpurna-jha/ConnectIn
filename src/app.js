7d6271ed314c1e30b1866f13af2ddd745ebdfc68
annpurna-jha <annpurnajha4@gmail.com>
2024-12-03 08:05:36 +0530

    handle error with try-catch

--- src/app.js
+++ src/app.js
@@ -1,24 +1,17 @@
 const express = require("express");
 
 const app = express();
-const {adminAuth, userAuth} = require('./middlewares/auth')
-// use of middleware - with help of this we don't have to write login of authentication again and again
-app.use("/admin",adminAuth); // auth for all routes /admin
 
-app.get("/user/getAllData",userAuth,(req,res)=>{ //when we don't want auth in all /user route
-    res.send("All data sent");
+//best way to handle error is using try-catch block
+app.get("/getUserData",(req,res)=>{   
+    try {
+        throw new error('dfhhf');//here error occurs so it will go to catch block. if no error occur then it send the response back.
+        res.send("All data sent");
+    } catch (error) {
+        res.status(500).send("Something went wrong. Contact Support team.")
+    }
+    
  })
-app.get("/user/signup",(req,res)=>{  // if we do auth check like /admin then auth check will happen in signup as well which is not needed
-    res.send("User signup successfully");
- })
-
-app.get("/admin/getAllData",(req,res)=>{
-   res.send("All data sent");
-})
-
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted a user");
-})
 
 app.listen(7777,()=>{ 
     console.log("server is listening");
