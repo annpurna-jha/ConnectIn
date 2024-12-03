6eb1a055cfce1d4129abc79ddb99f2724a567f8c
annpurna-jha <annpurnajha4@gmail.com>
2024-12-03 08:14:39 +0530

    wildcard-error-handler

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 
 const app = express();
-
-//best way to handle error is using try-catch block
 app.get("/getUserData",(req,res)=>{   
-    try {
-        throw new error('dfhhf');//here error occurs so it will go to catch block. if no error occur then it send the response back.
-        res.send("All data sent");
-    } catch (error) {
-        res.status(500).send("Something went wrong. Contact Support team.")
+        throw new error('dfhhf');
+ })
+//  for this express checks 4 arguments and treat 1st argument as error.if 3 argument is present it will treat 3rd argument as next . so 4 argument is must
+// this function should have in last so any error occur in code it cand handle it gracefully.
+ app.use("/", (err, req, res, next)=>{ //error handling middleware function
+    if(err){
+        // log error
+        res.status(500).send("Something went wrong!")
     }
-    
  })
 
 app.listen(7777,()=>{ 
