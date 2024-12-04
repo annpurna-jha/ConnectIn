9118ba7984c73f8985276f85dddeb602aabaf41c
annpurna-jha <annpurnajha4@gmail.com>
2024-12-04 17:10:20 +0530

    add-hardcode-data-into-db

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const connectDB=require('./config/database');
+const User = require('./models/user');
 
 const app = express();
-const connectDB=require('./config/database');
 
-//connnect db first then listing to server.right way to do it
+app.post("/signup",async (req,res)=>{
+//creating a new instance of User model
+    const user = new User({
+        firstName:"Annpurna",
+        lastName:"Jha",
+        emailId:"annpuna@jha.com",
+        password:"annpurna@123"
+    })
+try {
+    await user.save(); //return a promise that's why using async await
+    res.send("User added successfully!!");
+} catch (error) {
+    res.status(400).send("User can't be added");
+}
+})
+
+
+//connnect, db first, then listing to server.right way to do it
 connectDB().
 then(()=>{
     console.log("Database connected succesfully..");
