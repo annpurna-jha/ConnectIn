c7f2ce58e0f041fc544dd5bcc8c84306300f461b
annpurna-jha <annpurnajha4@gmail.com>
2024-12-06 09:03:57 +0530

    taking-data-from-endUser

--- src/app.js
+++ src/app.js
@@ -4,21 +4,23 @@ const User = require('./models/user');
 
 const app = express();
 
+app.use(express.json()); // will work for all api bcz route is not given, convert json to js object
+
 app.post("/signup",async (req,res)=>{
-//creating a new instance of User model
-    const user = new User({
-        firstName:"Annpurna",
-        lastName:"Jha",
-        emailId:"annpuna@jha.com",
-        password:"annpurna@123"
-    })
+
+//    console.log(req.body);//it will show undefined bcz it can't convert json to js object given to postman
+
+//creating a new User with the data getting from request
+    const user = new User(req.body);// taking dynamic data from end user here end user here end user is postman
+
 try {
     await user.save(); //return a promise that's why using async await
     res.send("User added successfully!!");
 } catch (error) {
     res.status(400).send("User can't be added");
 }
-})
+
+});
 
 
 //connnect, db first, then listing to server.right way to do it
