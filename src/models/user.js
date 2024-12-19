66e433fe7b9b4b359885bdcdfa779d07d9b78572
annpurna-jha <annpurnajha4@gmail.com>
2024-12-19 19:03:03 +0530

    creating-validatePassword-userSchema-methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
     {
@@ -77,8 +78,18 @@ const userSchema = new mongoose.Schema(
 userSchema.methods.getJWT = async function(){ // never user array function here
     const user = this;
     // creating token
-    const token = jwt.sign({_id:user._id},"Connect@In$790",{expiresIn:"7d"})
+    const token = await jwt.sign({_id:user._id},"Connect@In$790",{expiresIn:"7d"})
     return token;
 }
+
+userSchema.methods.validatePassword = async function(passwordInputByUser){
+    const user = this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash);
+    
+    return isPasswordValid;
+}
+
 const User = mongoose.model("User",userSchema);
 module.exports = User;
