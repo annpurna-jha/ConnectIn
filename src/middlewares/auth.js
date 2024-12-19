5eface80fbdd62940afa8adc823fa62dd3a8853f
annpurna-jha <annpurnajha4@gmail.com>
2024-12-19 11:16:44 +0530

    creating-userAuth-middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,14 +1,26 @@
-const adminAuth = (req,res,next)=>{
-    const token ="xyz";
-    const isAdminAuthorized = token =="xyz";
-    if(!isAdminAuthorized) res.status(401).send("Unauthorized request");
-    else next();
-}
-const userAuth = (req,res,next)=>{
-    const token ="xyz";
-    const isAdminAuthorized = token =="xyz";
-    if(!isAdminAuthorized) res.status(401).send("Unauthorized request");
-    else next();
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const userAuth = async (req,res,next)=>{
+
+    try{
+        const cookies = req.cookies;
+        const{token} = cookies;
+
+        if(!token) throw new Error("Invalid Token!!");
+
+        const decodedMessage = await jwt.verify(token,"Connect@In$790");
+        const {_id} = decodedMessage;
+
+        const user = await User.findById(_id);    
+
+        if(!user) throw new Error("User does not exist");
+
+        req.user = user;//send user data back
+        next();// calling request handler
+    }catch(err){
+        res.status(400).send("ERROR: "+err.message);
+    }
 }
 module.exports = 
-{adminAuth,userAuth};
+{userAuth};
