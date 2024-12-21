072d833d2468384faea76213907776511e9ddd6f
annpurna-jha <annpurnajha4@gmail.com>
2024-12-21 19:05:49 +0530

    create-profile-edit-api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
-
+const {validateEditProfileData} = require('../utils/validation');
 
 profileRouter.get("/profile/view",userAuth, async (req,res)=>{
 
@@ -14,5 +14,24 @@ profileRouter.get("/profile/view",userAuth, async (req,res)=>{
         res.status(400).send("ERROR : " + error.message);
     } 
 });
+
+profileRouter.patch("/profile/edit",userAuth, async (req,res)=>{
+    try {
+        if(!validateEditProfileData(req)) throw new Error("Invalid Edit Request");
+
+         const loggedInUser = req.user;
+
+         Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+         await loggedInUser.save();
+
+         res.json({
+            message:`${loggedInUser.firstName},Your profile updated successfuly`,
+            data: loggedInUser
+        });
+    } catch (error) {
+        res.status(400).send("ERROR : "+error.message);
+    }
+})
 
 module.exports = profileRouter;
