be7547d7090507dcc85c2af647fe90c5abab112a
annpurna-jha <annpurnajha4@gmail.com>
2024-12-21 19:05:09 +0530

    create-logout-api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -56,7 +56,6 @@ authRouter.post("/login",async (req,res)=>{
             // add the token to cookie and send the response back to the user
             res.cookie("token",token, 
                 {expires:new Date(Date.now() +  7 * 24 * 60 * 60 * 1000)}// 7 days from now
-                // {maxAge: 5000}
             );
             res.send("Login Successfull!");
 
@@ -64,7 +63,11 @@ authRouter.post("/login",async (req,res)=>{
     }catch (error) {
         res.status(400).send("ERROR : " + error.message);
     } 
-})
+});
 
+authRouter.post("/logout", (req,res)=>{
+    res.cookie("token",null,{expires:new Date(Date.now())});
+    res.send("Logout Successfull!!");
+});
 
 module.exports = authRouter;
